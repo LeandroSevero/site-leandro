347155bddb6a54a3e7755a37e80cc4f7140657f9
Leandro Severo <leandroolise@gmail.com>
2026-03-17 00:36:55 -0300

    Reset to start on right exit

--- main.js
+++ main.js
@@ -825,7 +825,7 @@ const initTimelineDot = () => {
 
       const shouldReset =
         goingLeft ||
-        (goingRight && isLast) ||
+        goingRight ||
         (goingUp && isFirst) ||
         (goingDown && isLast);
 
